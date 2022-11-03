74538db58417cbb8e7339d61c013165fc57c4f4b
Mark <mark@shockwavemedia.com.au>
2022-11-03 12:39:04 +0800

    - remove headers

--- routes/api.php
+++ routes/api.php
@@ -153,13 +153,6 @@ use App\Http\Controllers\API\Tickets\ListClientTicketController;
 use App\Http\Controllers\API\TicketEmails\CreateTicketEmailController;
 use App\Http\Controllers\API\Websites\ListClientWebsiteTicketsController;
 use Illuminate\Support\Facades\Route;
-
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
