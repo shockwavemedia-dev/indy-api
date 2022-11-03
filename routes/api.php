496800f48cfe41328d263f50920ab20b9f452258
Mark <mark@shockwavemedia.com.au>
2022-11-04 07:49:33 +0800

    - remove cors issue

--- routes/api.php
+++ routes/api.php
@@ -165,11 +165,6 @@ use Illuminate\Support\Facades\Route;
 */
 //
 
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-
 Route::get('/', function () {
     return [
         "data" => [
