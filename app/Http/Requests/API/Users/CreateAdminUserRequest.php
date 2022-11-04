96c532c3782c159f48dcd9f19a8e7e58c03c7d96
Mark <mark@shockwavemedia.com.au>
2022-11-04 09:02:41 +0800

    - update admin user request

--- app/Http/Requests/API/Users/CreateAdminUserRequest.php
+++ app/Http/Requests/API/Users/CreateAdminUserRequest.php
@@ -55,7 +55,7 @@ final class CreateAdminUserRequest extends AbstractCreateUserRequest
             'last_name' => 'string|nullable',
             'middle_name' => 'string|nullable',
             'password' => 'min:6|required_if:send_invite,false|required_with:password_confirmation|same:password_confirmation|nullable|string',
-            'password_confirmation' => 'min:6|required_if:send_invite,false',
+            'password_confirmation' => 'required_if:send_invite,false',
             'role' => [
                 'string',
                 Rule::in(AdminRoleEnum::toArray()),
