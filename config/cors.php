fe7a47d906cd3ba6d3d0f98005e18dc883f2fd0b
Mark <mark@shockwavemedia.com.au>
2022-11-04 09:25:22 +0800

    - update cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'api'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'api', '/*'],
 
     'allowed_methods' => ['*'],
 
