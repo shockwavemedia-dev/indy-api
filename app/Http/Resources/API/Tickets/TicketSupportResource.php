daf88150c0507f96545c6712b33ab564208a6c7c
Arjean Villodres <arjean.villodres@dailypress.com.au>
2022-11-23 13:19:07 +0800

    update extras format

--- app/Http/Resources/API/Tickets/TicketSupportResource.php
+++ app/Http/Resources/API/Tickets/TicketSupportResource.php
@@ -93,7 +93,7 @@ final class TicketSupportResource extends Resource
             $serviceTemp['service_id'] = $ticketService->getService()->getId();
             $serviceTemp['extras'] = $ticketService->getExtras();
             $serviceTemp['custom_fields'] = $ticketService->getCustomFields();
-            $serviceTemp['updatedExtras'] = $ticketService->getExtras();
+            $serviceTemp['updated_extras'] = $ticketService->getExtras();
             $result['services'][] = $serviceTemp;
         }
 
