2920bee3785cb805a9107d9b495a038a1241797f
Mark <mark@shockwavemedia.com.au>
2022-12-07 06:27:04 +0800

    - Remove error

--- app/Repositories/DepartmentRepository.php
+++ app/Repositories/DepartmentRepository.php
@@ -103,9 +103,6 @@ final class DepartmentRepository extends BaseRepository implements DepartmentRep
     public function findByName(string $name): ?Department
     {
         return $this->model->where('name', '=', $name)
-            ->whereHas('user', function($query) {
-                $query->whereNull('deleted_at');
-            })
             ->with('adminUsers')
             ->first();
     }
