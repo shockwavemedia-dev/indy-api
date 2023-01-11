5ce4144179c5ada98193cd7b1269b2831fed67d7
Mark <mark@shockwavemedia.com.au>
2023-01-11 08:02:10 +0800

    - removed comment in app boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -51,40 +51,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        $sentryHandler = new ErrorLog();
-//        Mailbox::catchAll(function (InboundEmail $email) use ($sentryHandler) {
-//            $sentryHandler->log($email->subject());
-//        });
-//
-//        Mailbox::from('design@indy.com.au', function (InboundEmail $email) use ($sentryHandler) {
-//            $sentryHandler->log($email->subject());
-//        });
-//
-//        Collection::macro('paginate', function ($perPage, $total = null, $page = null, $pageName = 'page') {
-//            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
-//
-//            return new LengthAwarePaginator(
-//                $total ? $this : $this->forPage($page, $perPage)->values(),
-//                $total ?: $this->count(),
-//                $perPage,
-//                $page,
-//                [
-//                    'path' => LengthAwarePaginator::resolveCurrentPath(),
-//                    'pageName' => $pageName,
-//                ]
-//            );
-//        });
+        $sentryHandler = new ErrorLog();
+        Mailbox::catchAll(function (InboundEmail $email) use ($sentryHandler) {
+            $sentryHandler->log($email->subject());
+        });
 
-//        if (!Collection::hasMacro('paginate')) {
-//
-//            Collection::macro('paginate',
-//                function ($perPage = 15, $page = null, $options = []) {
-//                    $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
-//                    return (new LengthAwarePaginator(
-//                        $this->forPage($page, $perPage), $this->count(), $perPage, $page, $options))
-//                        ->withPath('');
-//                });
-//        }
+        Mailbox::from('design@indy.com.au', function (InboundEmail $email) use ($sentryHandler) {
+            $sentryHandler->log($email->subject());
+        });
+
+        Collection::macro('paginate', function ($perPage, $total = null, $page = null, $pageName = 'page') {
+            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+
+            return new LengthAwarePaginator(
+                $total ? $this : $this->forPage($page, $perPage)->values(),
+                $total ?: $this->count(),
+                $perPage,
+                $page,
+                [
+                    'path' => LengthAwarePaginator::resolveCurrentPath(),
+                    'pageName' => $pageName,
+                ]
+            );
+        });
 
         Schema::defaultStringLength(191);
     }
